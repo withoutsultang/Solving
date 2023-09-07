26e1a6663063b0019a94e94ada4424d0f90e99de
withoutSugar <120733105+withoutsultang@users.noreply.github.com>
2023-09-07 23:35:16 +0900

    [Bronze V] Title: A-B, Time: 208 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        String s = sc.nextLine();
+
+        String[] sA = s.split(" ");
+
+        int a = Integer.parseInt(sA[0]);
+        int b = Integer.parseInt(sA[1]);
+
+
+        System.out.println(a - b);
+  
+
+
+    }
+}
