9afd8af871a31c5512d7f7a950a8da9b37851ee1
김건우 <120733105+withoutsultang@users.noreply.github.com>
2026-04-03 14:34:59 +0900

    [Silver IV] Title: 설탕 배달, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,19 @@
+import sys
+def input(): return sys.stdin.readline()
+
+n = int(input())
+
+count = 0
+
+while n >= 0:
+    if n % 5 == 0:
+        count += n // 5
+        break
+    else:
+        n -= 3
+        count += 1
+
+if n < 0:
+    print(-1)
+else:
+    print(count)
